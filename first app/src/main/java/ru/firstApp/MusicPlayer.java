4905f6f6331b0b71c36dddd318f83e24efd6b1fe
Egor <giksengik@gmail.com>
2021-01-06 09:33:56 +0300

    lesson 5

--- first app/src/main/java/ru/firstApp/MusicPlayer.java
+++ first app/src/main/java/ru/firstApp/MusicPlayer.java
@@ -2,10 +2,36 @@ package ru.firstApp;
 
 public class MusicPlayer {
     private Music music;
+
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public MusicPlayer(){
+
+    }
     public MusicPlayer(Music music){
         this.music=music;
     }
     public void playMusic(){
         System.out.println("Playing: "+this.music.getSong());
+    }
+    public void setMusic(Music music){
+        this.music=music;
     }
 }
