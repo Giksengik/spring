341356639bbc1f16e4f172f4cbeee952324cb8fd
Egor <giksengik@gmail.com>
2021-01-06 08:41:33 +0300

    lesson 4

--- first app/src/main/java/ru/firstApp/TestSpring.java
+++ first app/src/main/java/ru/firstApp/TestSpring.java
@@ -6,8 +6,7 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context =  new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        Music music =context.getBean("musicBean",Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer= context.getBean("musicPlayer",MusicPlayer.class);
         musicPlayer.playMusic();
         context.close();
     }
