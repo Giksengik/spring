2d029342f93c64137aafc02948df386185a28f5a
Egor <giksengik@gmail.com>
2021-01-07 15:59:30 +0300

    lesson 10

--- first app/src/main/java/ru/firstApp/MusicPlayer.java
+++ first app/src/main/java/ru/firstApp/MusicPlayer.java
@@ -1,21 +1,23 @@
 package ru.firstApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
+    private Music music;
+    private Music music2;
     private String name;
     private int volume;
 @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
+    public MusicPlayer(@Qualifier("rockMusic") Music music,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music=music;
+        this.music2=music2;
     }
     public String playMusic(){
-        return "Playing: "+this.classicalMusic.getSong();
+        return "Playing: "+this.music.getSong() +" and " +this.music2.getSong();
+
     }
 }
