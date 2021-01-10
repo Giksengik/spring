67fe3b1fb9041903748810e8103e4defc81e96b1
Egor <giksengik@gmail.com>
2021-01-10 18:32:31 +0300

    lesson 20 pt1

--- MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/controllers/FirstController.java
+++ MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.vlasov.MVCFirstApp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,9 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
     @GetMapping("hello")
     public String helloPage(@RequestParam(value = "name",required = false) String name ,
-                            @RequestParam(value = "surname",required = false) String surname) {
-        System.out.println("Hello "+name+" "+surname);
+                            @RequestParam(value = "surname",required = false) String surname,
+                            Model model) {
 
+        //System.out.println("Hello "+name+" "+surname);
+        model.addAttribute("message","Hello "+name+" "+surname);
         return("first/hello");
     }
     @GetMapping("goodbye")
