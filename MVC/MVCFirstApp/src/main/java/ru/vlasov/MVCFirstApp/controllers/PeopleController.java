aa5a0465b0b33ece937cf52976e9c6fb9192718a
Egor <giksengik@gmail.com>
2021-01-10 20:17:57 +0300

    lesson 21

---
+++ MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/controllers/PeopleController.java
@@ -0,0 +1,33 @@
+package ru.vlasov.MVCFirstApp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.vlasov.MVCFirstApp.dao.PersonDao;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDao personDAO;
+
+    @Autowired
+    public PeopleController(PersonDao personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
---
+++ MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/dao/PersonDao.java
@@ -0,0 +1,27 @@
+package ru.vlasov.MVCFirstApp.dao;
+
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+import org.springframework.stereotype.Component;
+import ru.vlasov.MVCFirstApp.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class PersonDao {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+    {
+        people=new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT,"Tom "));
+        people.add(new Person(++PEOPLE_COUNT,"Bob"));
+        people.add(new Person(++PEOPLE_COUNT,"Mike"));
+        people.add(new Person(++PEOPLE_COUNT,"Katy"));
+    }
+    public List<Person> index(){
+        return people;
+    }
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null);
+
+    }
+}
