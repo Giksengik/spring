9d52d0eaeefc97d1f5e727fc111fd22eb33c95c2
Egor <giksengik@gmail.com>
2021-01-11 21:00:14 +0300

    lesson 23

--- MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/dao/PersonDao.java
+++ MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/dao/PersonDao.java
@@ -28,4 +28,11 @@ public class PersonDao {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
+    public void update(int id,Person person){
+       Person personToBeUpdated = show(id);
+       personToBeUpdated.setName(person.getName());
+    }
+    public void delete(int id){
+        people.removeIf(p->p.getId()==id);
+    }
 }
