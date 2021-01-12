c1c9d05109b0536e4b2dd55544b447294c670196
Egor <giksengik@gmail.com>
2021-01-12 13:09:50 +0300

    lesson 24

--- MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/controllers/PeopleController.java
+++ MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/controllers/PeopleController.java
@@ -3,9 +3,11 @@ package ru.vlasov.MVCFirstApp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.vlasov.MVCFirstApp.dao.PersonDao;
 import ru.vlasov.MVCFirstApp.models.Person;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -23,26 +25,28 @@ public class PeopleController {
         model.addAttribute("people", personDAO.index());
         return "people/index";
     }
-    @PostMapping("/new")
-    public String createRandom(){
-        personDAO.createRandom();
-        return "redirect:/people";
-    }
+
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
     }
+
     @GetMapping("/new")
-    public String newPerson(Model model){
-        model.addAttribute("person",new Person());
+    public String newPerson(@ModelAttribute("person") Person person) {
         return "people/new";
     }
+
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
+
         personDAO.save(person);
         return "redirect:/people";
     }
+
     @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("person", personDAO.show(id));
@@ -50,18 +54,18 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
+
     @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
         personDAO.delete(id);
-        return "redirect:/people";
-    }
-    @PostMapping("/{id}")
-    public String clone(@PathVariable("id") int id){
-        personDAO.clone(id);
         return "redirect:/people";
     }
 }
--- MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/dao/PersonDao.java
+++ MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/dao/PersonDao.java
@@ -12,45 +12,38 @@ import java.util.Random;
 public class PersonDao {
     private static int PEOPLE_COUNT;
     private List<Person> people;
-    private List<String> names;
+
     {
-        people=new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT,"Tom "));
-        people.add(new Person(++PEOPLE_COUNT,"Bob"));
-        people.add(new Person(++PEOPLE_COUNT,"Mike"));
-        people.add(new Person(++PEOPLE_COUNT,"Katy"));
-        names= new ArrayList<>();
-        names.add(new String("Egor"));
-        names.add(new String("Michael"));
-        names.add(new String("Sheldon"));
-        names.add(new String("Leonard"));
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 24, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 52, "bob@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike", 18, "mike@yahoo.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy", 34, "katy@gmail.com"));
     }
-    public List<Person> index(){
+
+    public List<Person> index() {
         return people;
     }
-    public Person show(int id){
-        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null);
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
-    public void save(Person person){
+
+    public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
     }
-    public void update(int id,Person person){
-       Person personToBeUpdated = show(id);
-       personToBeUpdated.setName(person.getName());
-       personToBeUpdated.setAge(person.getAge());
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
-    public void delete(int id){
-        people.removeIf(p->p.getId()==id);
-    }
-    public void clone(int id){
-        Person personToBeCloned = show(id);
-        personToBeCloned.setId(++PEOPLE_COUNT);
-        people.add(personToBeCloned);
-    }
-    public void createRandom(){
-        Person personToAdd= new Person();
-        personToAdd.setName("Egor");
-        save(personToAdd);
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
     }
 }
