4f5d60e8832d8933bc50f98fda22b3586e75fa43
Egor <giksengik@gmail.com>
2021-01-13 21:43:39 +0300

    lesson 26

--- MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/dao/PersonDao.java
+++ MVC/MVCFirstApp/src/main/java/ru/vlasov/MVCFirstApp/dao/PersonDao.java
@@ -33,6 +33,15 @@ public class PersonDao {
         List<Person> people = new ArrayList<Person>();
         try {
             Statement statement= connection.createStatement();
+            String CMND="SELECT MAX(id)as max FROM Person";
+            ResultSet resultSet = statement.executeQuery(CMND);
+            resultSet.next();
+            PEOPLE_COUNT=resultSet.getInt("max");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try {
+            Statement statement= connection.createStatement();
             String SQL="SELECT * FROM Person";
             ResultSet resultSet = statement.executeQuery(SQL);
             while(resultSet.next()){
@@ -50,16 +59,35 @@ public class PersonDao {
     }
 
     public Person show(int id) {
-//        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
-        return null;
+        Person person=null;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement
+                    ("SELECT * FROM PERSON WHERE id=?");
+            preparedStatement.setInt(1,id);
+            ResultSet resultSet =preparedStatement.executeQuery();
+            resultSet.next();
+            person=new Person();
+            person.setId(resultSet.getInt("id"));
+            person.setAge(resultSet.getInt("age"));
+            person.setEmail(resultSet.getString("email"));
+            person.setName(resultSet.getString("name"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return person;
+
     }
 
     public void save(Person person) {
         try {
-            Statement statement= connection.createStatement();
-            String SQL="INSERT INTO Person VALUES("+(++PEOPLE_COUNT)+",'"+person.getName()+
-                    "',"+person.getAge()+",'"+person.getEmail()+"')";
-            statement.executeUpdate(SQL);
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO Person VALUES(?,?,?,?)");
+            preparedStatement.setInt(1,++PEOPLE_COUNT);
+            preparedStatement.setString(2,person.getName());
+            preparedStatement.setInt(3,person.getAge());
+            preparedStatement.setString(4,person.getEmail());
+            preparedStatement.executeUpdate();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -67,14 +95,28 @@ public class PersonDao {
     }
 
     public void update(int id, Person updatedPerson) {
-//        Person personToBeUpdated = show(id);
-//
-//        personToBeUpdated.setName(updatedPerson.getName());
-//        personToBeUpdated.setAge(updatedPerson.getAge());
-//        personToBeUpdated.setEmail(updatedPerson.getEmail());
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "UPDATE Person SET name=?, age=?,email=? WHERE id=?");
+            preparedStatement.setString(1,updatedPerson.getName());
+            preparedStatement.setInt(2,updatedPerson.getAge());
+            preparedStatement.setString(3,updatedPerson.getEmail());
+            preparedStatement.setInt(4,id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void delete(int id) {
-//            people.removeIf(p -> p.getId() == id);
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement(
+                    "DELETE FROM Person WHERE id=?");
+            preparedStatement.setInt(1,id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
